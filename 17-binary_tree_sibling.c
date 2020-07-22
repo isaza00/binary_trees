9aa0397a21c5b540470713b5bd074936318318fc
Norman <isaza00@hotmail.com>
2020-07-21 19:40:38 -0500

    17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
 		return (NULL);
-	if (node->parent->left->n == node->n)
+	if (node->parent->left && node->parent->left->n == node->n)
 		return (node->parent->right);
 	return (node->parent->left);
 }
